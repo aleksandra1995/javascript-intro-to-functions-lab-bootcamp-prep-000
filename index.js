e3d5af0612da20ac72b14a21495a2940e24e1c63
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-02 21:33:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ function shout(string)
 function whisper(string)
 {return string.toLowerCase() }
 function logShout(string) 
-{console.log( string.toUpperCase) }
+{console.log( `$(string.toUpperCase)` }
